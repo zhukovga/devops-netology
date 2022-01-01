533aedddf718ec457e8d16ab5dfade7c7818f6fe
Zhukov George <zhukovga@live.ru>
2022-01-01 16:53:16 +1100

    git-rebase 2

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -4,4 +4,7 @@
 count=1
 for param in "$@"; do
 	    echo "\$@ Parameter #$count = $param"
-	    echo "Parameter: $param"
+	    echo "Next parameter: $param"
+	        count=$(( $count + 1 ))
+	done
+	echo "====="
